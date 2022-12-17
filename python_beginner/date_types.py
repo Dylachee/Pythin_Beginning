915c0e381dca55ab98ae115367222ffdd1237d63
Dylachee <saaakbaevv@gmail.com>
2022-12-17 13:05:01 +0600

    First

---
+++ python_beginner/date_types.py
@@ -0,0 +1,86 @@
+
+# 8 main data type , which seperate to 2 types. 
+# 1) Changeble 
+# 2) Unchangeble
+
+# Data types;
+# 1) int
+
+#     a float 
+#     b) compilex 
+
+# 2) str - string data type 
+# 3) bool - locals data type ( he can be "try or false")
+# 4) list - he can save different data types ([])
+# 5) tuple - same list but different in that it can ba changed ()
+# 6) set - can save unical things({})
+# 7) dict - dictionary ({}). 
+# 8) NoneType - for showing existing data types (None)
+
+# In all of this data type only 3 of them can be changed (mutable)
+# list
+# dict
+# set
+# print("Hello, world!")
+# a = 1
+# print(a)
+
+# - one srt comment
+""" - multiple srt comments """
+
+#Variables
+
+#a = 1 # That good! 
+
+# !@%^&msd = Error
+# 123fw = Error
+
+# Name of the variable must be start with numbers or special characters
+
+# my_num = 20 that good
+# myNum = CamelCase
+
+# if variable has one or more characters you cannot use "-" or with space 
+
+# name of variable must not be the same as main variable in python ( print = 10 # error)
+
+# if = 10 # error
+
+
+# IF variable named with big word it mean that it is constant!  (MY_CONST)
+
+# Name your variable like you giving name to your son 
+
+# print - function for conclining input for inputing to reminal 
+
+# print() # function for conclining information to terminal
+# input() # function for inputting information from keyboard to terminal. Str data type.
+# type() # function for conclining sharing data type to her. 
+# () = call function , also serving as container for data sharing
+
+# info = input("type numbers")
+# print(type(info))
+
+# get_number = int(input())
+# print(type(get_number))
+
+# get_first_number = int(input('1'))
+# get_second_number = int(input('2'))
+# result = get_first_number + get_second_number
+# print("Answer is ", result)
+
+
+# y = 7 
+# m = 12
+# d = 365
+# totalm = y * m
+
+# totald = y * d
+# print(totalm)
+# print(totald)
+
+x =  int(10)
+y =  int(13)
+
+res =  x / y
+print(res)
---
+++ python_beginner/integers.py
@@ -0,0 +1,119 @@
+# int() - integers (1, 2, 3)
+# int_num = 20 
+# float() - floats (1.4 , 2.4, 3.4)
+# float_num = 20.4
+"""Operators for working with numbers / math operations"""
+
+# num1 = 10
+# num2 = 5
+
+# num1 + num2 #addition 
+# num1 - num2 #subtraction 
+# num1 * num2 #multiplication 
+# num1 / num2 #division 
+# num1 // num2 #integer division 
+# num1 % num2 #modulo ---> int
+# num1 ** num2 #exponentiation
+
+# result = 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1 + 0.1
+# print(result)
+
+# from decimal import Decimal
+# decimal_1 = Decimal("0.1")
+# decimal_2 = Decimal("0.1")
+# decimal_3 = Decimal("0.1")
+
+# print(decimal_1 + decimal_2 + decimal_3)
+
+# odd = 5round(5.2) # 5
+# round(-5.2) # -5
+# round(5.5) # 5.5
+# round(-5.5) # -5.5
+# round(5.2, 2) # 5.2
+# even = 4
+
+# print(even % 2)
+# print(odd % 2)
+
+""" Function for working with numbers"""
+
+# abs(x) = absolute value. This function returns transmitted value/numbers with respect
+
+
+# print(abs(5)) # 5
+# print(abs(-6)) # 6
+# print(abs(5.5)) # 5.5
+# print(abs(-6.5)) # 6.5
+
+# pow =
+
+# print(pow(2, 3)) # 2** 3 = 8
+# print (pow(2, 3, 4))# 2 ** 3 % 4 = 0
+
+# print(divmod(1, 2)) # 1 // 2 , 1%2 --> (0, 1)
+
+# divmod(x , y)
+
+# round() = rounding
+
+# round(5.2) # 5
+# round(-5.2) # -5
+# round(5.5) # 5.5
+# round(-5.5) # -5.5
+# round(5.2, 2) # 5.2
+
+# num_3 = 10
+# print(float(num_3))
+
+# num_4 = 10.4
+# print(int(num_4))
+
+# counter = 0
+# counter += 1 # counter - counter + (increment- dicrement)
+
+# import math
+
+# math.sqrt(25)
+# math.factorial(5)
+# math.pi 
+"""task 10"""
+# inp1 = input(int())
+# inp2 = input(int())
+# inp3 = input(intdecimal = 23
+
+
+# fres = int(inp1) * int(inp2) % int(inp3)
+
+# print(fres)
+
+"""task 11"""
+
+# num1 = input()
+# num2 = input()inp1 = input(int())
+# inp2 = input(int())
+# inp3 = input(int())
+
+# fres = int(inp1) * int(inp2) % int(inp3)
+
+# print(fres)
+# num3 = float(3.4)
+
+# res = int(num1) % int(num2) * float(num3)
+
+# print(res)
+import math
+
+"""В переменные leg_a, leg_b запишите два числа, которые будут обозначать два катета прямоугольного треугольника .
+
+Рассчитайте длину гипотенузы треугольника hypotenuse, воспользовавшись теоремой Пифагора.
+Note: Теорема Пифагора:
+
+ a ** 2 + b ** 2 = c ** 2.
+
+"""
+from math import hypot ,sqrt
+leg_a = 4 
+leg_b = 5 
+leg_c = pow(leg_a, 2) + pow(leg_b, 2) 
+hypotenuse = hypot(sqrt(leg_c)) 
+print(hypotenuse)
